9e95cfe66f5706a81a0acd4408b8dc1492a3a7ce
Taylor Engel <tengel92@gmail.com>
2020-10-06 17:47:26 -0500

    Added standard for loop

--- javascript/loops.js
+++ javascript/loops.js
@@ -7,7 +7,7 @@ const delay = (ms) => {
   return new Promise((resolve) => setTimeout(resolve, ms));
 };
 
-const simpleArray = ['string1', 'string2'];
+const simpleArray = ['string1', 'string2', 'string3', 'string4', 'string5'];
 
 const forEachExample = async () => {
   const startForEach = performance.now();
@@ -24,21 +24,38 @@ const forEachExample = async () => {
   log.performance(`Total of forEach`, startForEach, endForEach);
 };
 
+const forExample = async () => {
+  const startFor = performance.now();
+  for (let i = 0; i < simpleArray.length; i++) {
+    const start = performance.now();
+
+    await delay(1000);
+    log.result(`This is  `, simpleArray[i]);
+
+    const end = performance.now();
+    log.performance(`for loop`, start, end);
+  }
+
+  const endFor = performance.now();
+  log.performance(`total of for loop`, startFor, endFor);
+};
+
 const forOfExample = async () => {
   const startForOf = performance.now();
   for (const item of simpleArray) {
     const start = performance.now();
 
-    await delay(1000);
+    await delay(10);
     log.result(`This is  `, item);
 
     const end = performance.now();
-    log.performance(`forEach`, start, end);
+    log.performance(`for of`, start, end);
   }
 
   const endForOf = performance.now();
   log.performance(`total of for of`, startForOf, endForOf);
 };
 
-forEachExample();
+// forEachExample();
+// forExample();
 forOfExample();
